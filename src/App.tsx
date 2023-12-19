0effb450c8ad842a9d98c061409df1ef931553fd
nasuda <nasuda908@gmail.com>
2023-12-19 20:39:14 +0900

    add App.tsx new functions

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,39 @@ const user = {
   line: 'boshiboshi',
 }
 
+const titleStyle = {
+  color: 'blue',
+  border: 'dotted',
+}
+
+let red = 'red'
+
+
+let isTrue = true;
+function Crender() {
+  let content;
+  if (isTrue) {
+    content = <p>true</p>
+  } else {
+    content = <p>false</p>
+  }
+  return (
+    <div>
+      {content}
+    </div>
+  )
+}
 export default function App() {
   return (
     <div>
       <h1>{user.name}</h1>
-      <h2>lines</h2>
+      <h2 style={{ color: titleStyle.color, border: titleStyle.border }}>lines</h2>
+      <p style={{color: red}}>red moji</p>
       <ul>
         <li>{user.line}</li>
       </ul>
       <MyButton />
+      <Crender />
     </div>
   )
 }
